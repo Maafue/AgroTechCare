2e6d14e663e3a84a4a77db090a9b86bb28ee583b
Moroz Maksim <morozmaksim99@mail.ru>
2025-05-26 13:18:29 +0300

    Initial commit

---
+++ src/main/java/by/morozmaksim/AgroTechCareApp/AgroTechCareApplication.java
@@ -0,0 +1,13 @@
+package by.morozmaksim.AgroTechCareApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroTechCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroTechCareApplication.class, args);
+	}
+
+}
